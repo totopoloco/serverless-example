9f6e70e055387fc1843248467e1f5a6fb94adc0f
Marco Tulio Ávila Cerón <marcotavilac@exatec.tec.mx>
2020-10-15 19:07:05 +0200

    [none] Completed demo

--- helloworld/src/main/java/helloworld/HelloWorldController.java
+++ helloworld/src/main/java/helloworld/HelloWorldController.java
@@ -4,13 +4,18 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/hello")
 public class HelloWorldController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldController.class);
+
     @Get
     @Produces(MediaType.TEXT_PLAIN)
     public String sayIt() {
+        LOGGER.info("Called service");
         return "Hi there";
     }
 
